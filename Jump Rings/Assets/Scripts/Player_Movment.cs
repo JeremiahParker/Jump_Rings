970d0afbcf9f73fabda659ee4ebf47de0a5ad597
JeremiahParker <46364482+JeremiahParker@users.noreply.github.com>
2019-01-18 15:08:14 -0600

    Jump_Rings(002)
    BackUp 002

--- Jump Rings/Assets/Scripts/Player_Movment.cs
+++ Jump Rings/Assets/Scripts/Player_Movment.cs
@@ -13,25 +13,30 @@ public class Player_Movment : MonoBehaviour
     //Force applyed to the right of the player when the space bar is pressed
     [Header("Vertical speed")]
     [Tooltip("Force applyed to the negitive x axis when space bar is pressed")]
-    [Range(0f, 1000f)]
+    [Range(0f, 10000f)]
     public float Vertical_Movment;
     //Force applyed to the bottom of the player when the space bar is pressed
     [Header("Horizontal speed")]
     [Tooltip("Force applyed to the positive y axis when space bar is pressed")]
-    [Range(0f, 1000f)]
+    [Range(0f, 10000f)]
     public float Horizontal_Movment;
+    //Checks if the player is pressing space
+
     #endregion
-    #region UpdateMethod
+    #region FixedUpdateMethod
 
     //Code within will run every fixed frame
      void FixedUpdate(){
-
+        //Checks if the player presses space, if this is so the code within will run
         if (Input.GetKey("space")){
 
-            RB.AddForce(-Horizontal_Movment * Time.deltaTime, Vertical_Movment * Time.deltaTime, 0);
+            RB.AddForce(0, Vertical_Movment * Time.deltaTime, -Horizontal_Movment * Time.deltaTime);
 
         }
-
+        
     }
     #endregion
+    void Update(){
+        
+    }
 }
